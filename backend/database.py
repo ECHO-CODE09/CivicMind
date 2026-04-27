84d19ea1591722ad43001675855b06aee4cfe500
ECHO-CODE09 <kunalkuma871@gmail.com>
2026-04-27 18:12:49 +0530

    updated some files

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://postgres:7823@localhost:5432/civicmind")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
